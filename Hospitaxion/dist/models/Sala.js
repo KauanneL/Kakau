31e21b236c218834f10f5d87191af31ddc6c8adb
KauanneL <137060290+KauanneL@users.noreply.github.com>
2025-01-16 21:36:14 -0300

    delete /dist

--- Hospitaxion/dist/models/Sala.js
+++
@@ -1,8 +0,0 @@
-export class Sala {
-    constructor(numero, data, horario, status) {
-        this.numero = numero;
-        this.data = data;
-        this.horario = horario;
-        this.status = status;
-    }
-}
